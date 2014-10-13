b7e46a1aebb6bfa71495ef22749bba7de700adea
naleite <naleite.nuaa@gmail.com>
2014-10-13 11:34:01 +0200

    Maven convert

---
+++ src/aspect/LogAspect.java
@@ -0,0 +1,16 @@
+package aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class LogAspect {
+	@Before("execution(public * *.*(..))")
+	//@AfterReturning("execution(* *.*(..))")
+	public void log(JoinPoint pjp) throws Throwable{
+		System.out.println("log " + pjp.getSignature().getName() );//+pjp.getThis().toString());
+		
+	}
+
+}
